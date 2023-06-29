14a9b6a7c0184576379b46f77a7892bb8d7e31b3
The2009Zapdos <138090038+The2009Zapdos@users.noreply.github.com>
2023-06-29 01:26:00 -0400

    Update make moves.ts

--- data/mods/petmod-creator/make moves.ts
+++ data/mods/petmod-creator/make moves.ts
@@ -2,4 +2,22 @@
 
   // THIS IS WHERE YOU ADD NEW / EDITED MOVES
 
-};
+firelash: {
+        num: 680,
+        accuracy: 100,
+        basePower: 80,
+        category: "Physical",
+        name: "Fire Lash",
+        pp: 15,
+        priority: 0,
+        flags: {contact: 1, protect: 1, mirror: 1},
+        secondary: {
+            chance: 100,
+            boosts: {
+                def: -1,
+            },
+        },
+        target: "normal",
+        type: "Fire",
+        contestType: "Cute",
+    };
