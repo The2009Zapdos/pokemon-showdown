d08d98f8ac456aa5a5275067f52491aa003f1d51
The2009Zapdos <138090038+The2009Zapdos@users.noreply.github.com>
2023-06-29 01:52:09 -0400

    Create learnsets.ts

---
+++ data/mods/petmod-creator/learnsets.ts
@@ -0,0 +1,11 @@
+{[speciesid: string]: LearnsetData} = {
+
+  // YOU GET THE IDEA I THINK
+
+{learnset: {
+    electroball: ["8L1"],
+    highjumpkick: ["8L1"], 
+    thunderwave: ["8L1"],
+    rest: ["8L1"],
+    sleeptalk: ["8L1"],  
+  }},
