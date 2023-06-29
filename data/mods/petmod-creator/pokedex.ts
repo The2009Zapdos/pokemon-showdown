1125b99674fc0f084df445c6730ac893d4fe7325
The2009Zapdos <138090038+The2009Zapdos@users.noreply.github.com>
2023-06-29 00:56:42 -0400

    Create pokedex.ts

---
+++ data/mods/petmod-creator/pokedex.ts
@@ -0,0 +1,48 @@
+{[k: string]: ModdedSpeciesData} = {
+
+  // THIS IS WHERE NEW / EDITED POKEMON DATA GOES
+ 
+rotom: {
+        num: 479,
+        name: "Rotom",
+        types: ["Electric", "Ghost"],
+        gender: "N",
+        baseStats: {hp: 50, atk: 50, def: 77, spa: 95, spd: 77, spe: 91},
+        abilities: {0: "Levitate"},
+        heightm: 0.3,
+        weightkg: 0.3,
+        color: "Red",
+        eggGroups: ["Amorphous"],
+        otherFormes: ["Rotom-Heat", "Rotom-Wash"],
+        formeOrder: ["Rotom", "Rotom-Heat", "Rotom-Wash""],
+    },
+    rotomheat: {
+        num: 479,
+        name: "Rotom-Heat",
+        baseSpecies: "Rotom",
+        forme: "Heat",
+        types: ["Electric", "Fire"],
+        gender: "N",
+        baseStats: {hp: 50, atk: 65, def: 107, spa: 105, spd: 107, spe: 86},
+        abilities: {0: "Levitate"},
+        heightm: 0.3,
+        weightkg: 0.3,
+        color: "Red",
+        eggGroups: ["Amorphous"],
+        changesFrom: "Rotom",
+    },
+    rotomwash: {
+        num: 479,
+        name: "Rotom-Wash",
+        baseSpecies: "Rotom",
+        forme: "Wash",
+        types: ["Electric", "Water"],
+        gender: "N",
+        baseStats: {hp: 50, atk: 65, def: 107, spa: 105, spd: 107, spe: 86},
+        abilities: {0: "Levitate"},
+        heightm: 0.3,
+        weightkg: 0.3,
+        color: "Red",
+        eggGroups: ["Amorphous"],
+        changesFrom: "Rotom",
+    };
